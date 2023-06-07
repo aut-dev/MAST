4c7e65ab633a7f2ead19bba571775d320a933298
Boris Blondin <boris@puzzlers.run>
2023-06-07 17:30:21 +0100

    payment method changes [skip npm]

--- plugins/stripe/src/behaviors/UserBehavior.php
+++ plugins/stripe/src/behaviors/UserBehavior.php
@@ -9,8 +9,31 @@ class UserBehavior extends Behavior
 {
     public $owner;
 
-    public function getPaymentMethods(): array
+    public function getHasValidPaymentMethod()
     {
-        return Stripe::$plugin->stripe->getPaymentMethods($this->owner);
+        $method = Stripe::$plugin->stripe->getPaymentMethod($this->owner);
+        if (!$method) {
+            return false;
+        }
+        $card = $method->card;
+        $now = $this->owner->now;
+        if ($card->exp_year < $now->format('Y')) {
+            return false;
+        }
+        if ($card->exp_year == $now->format('Y')) {
+            return ($card->exp_month > $now->format('n'));
+        }
+        return true;
+    }
+
+    public function getPaymentMethodExpiresSoon()
+    {
+        $method = Stripe::$plugin->stripe->getPaymentMethod($this->owner);
+        if (!$method) {
+            return false;
+        }
+        $card = $method->card;
+        $now = $this->owner->now;
+        return ($card->exp_year == $now->format('Y') and $card->exp_month == ($now->format('n') - 1));
     }
 }
--- plugins/stripe/src/services/StripeService.php
+++ plugins/stripe/src/services/StripeService.php
@@ -3,6 +3,7 @@
 namespace Plugins\Stripe\services;
 
 use Stripe\Customer;
+use Stripe\PaymentMethod;
 use Stripe\SetupIntent;
 use Stripe\StripeClient;
 use craft\base\Component;
@@ -10,6 +11,8 @@ use craft\elements\User;
 
 class StripeService extends Component
 {
+    public const PAYMENT_METHOD_CACHE_KEY = 'stripe-payment-method-';
+
     protected $client;
 
     public function getOrCreateCustomer(User $user): Customer
@@ -22,21 +25,6 @@ class StripeService extends Component
         } else {
             return $this->createCustomer($user);
         }
-    }
-
-    public function getPaymentMethods(User $user): array
-    {
-        if (!$user->stripeCustomer) {
-            return [];
-        }
-        $methods = [];
-        foreach ($this->getClient()->paymentMethods->all([
-            'customer' => $user->stripeCustomer,
-            'type' => 'card'
-        ]) as $method) {
-            $methods[] = $method;
-        }
-        return $methods;
     }
 
     public function createCustomer(User $user): Customer
@@ -66,8 +54,27 @@ class StripeService extends Component
             $user = \Craft::$app->user->identity;
             $user->setFieldValue('paymentMethod', $intent->payment_method);
             \Craft::$app->elements->saveElement($user, false);
+            $this->clearPaymentMethodCache($user);
         }
         return $intent;
+    }
+
+    public function getPaymentMethod(User $user): ?PaymentMethod
+    {
+        if (!$user->paymentMethod or!$user->stripeCustomer) {
+            return null;
+        }
+        $data = \Craft::$app->cache->get(self::PAYMENT_METHOD_CACHE_KEY . $user->id);
+        if ($data === false) {
+            $data = $this->getClient()->customers->retrievePaymentMethod($user->stripeCustomer, $user->paymentMethod);
+            \Craft::$app->cache->set(self::PAYMENT_METHOD_CACHE_KEY . $user->id, $data, 86400);
+        }
+        return $data;
+    }
+
+    public function clearPaymentMethodCache(User $user)
+    {
+        \Craft::$app->cache->delete(self::PAYMENT_METHOD_CACHE_KEY . $user->id);
     }
 
     protected function getClient(): StripeClient
