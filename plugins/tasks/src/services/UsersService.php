d9d5b14b59e0bbf9f23c84cd69afa18cc4315752
Boris Blondin <boris@puzzlers.run>
2023-07-20 07:07:04 +0100

    change timezone fix

--- plugins/tasks/src/services/UsersService.php
+++ plugins/tasks/src/services/UsersService.php
@@ -23,29 +23,20 @@ class UsersService extends Component
             $tasks = Entry::find()->section('task')->authorId($user->id)->anyStatus()->all();
             $today = $user->today;
             $oldToday = (new DateTime())->setTimezone(new DateTimeZone($old->timezone))->setTime(0, 0, 0);
-            $dayHasChanged = ($today->format('d-m-Y') != $oldToday->format('d-m-Y'));
             foreach ($tasks as $task) {
-                if ($dayHasChanged) {
-                    //Day has changed, need to delete the daily task and create it again or we could have duplicates or daily tasks that are in the future
-                    $daily = Tasks::$plugin->tasks->getDailyTask($task, $oldToday);
-                    if ($daily and !$daily->processed) {
-                        \Craft::$app->elements->deleteElement($daily, true);
-                        Tasks::$plugin->tasks->getOrCreateDailyTask($task, $today);
-                    }
-                }
                 $date = (clone $task->startDate)
                     ->setTimezone(new DateTimeZone($user->timezone))
                     ->setDate($task->startDate->format('Y'), $task->startDate->format('m'), $task->startDate->format('d'))
                     ->setTime(0, 0, 0);
-                $task->startDate = $date;
+                $daily = Tasks::$plugin->tasks->getDailyTask($task, $oldToday);
+                $task->setFieldValue('startDate', $date);
                 \Craft::$app->elements->saveElement($task, false);
-                if ($dayHasChanged) {
-                    //Now we need to make sure the daily task is updated, the day has changed so the schedule could make the length different for the new day
-                    $daily = Tasks::$plugin->tasks->getOrCreateDailyTask($task, $today);
-                    if ($daily and !$daily->processed) {
-                        $daily->setFieldValue('length', $task->getDuration($today));
-                        \Craft::$app->elements->saveElement($daily, false);
-                    }
+                if ($daily and !$daily->processed) {
+                    $daily->setFieldValues([
+                        'startDate' => $today,
+                        'length' => $task->getDuration($today)
+                    ]);
+                    \Craft::$app->elements->saveElement($daily, false);
                 }
             }
         }
